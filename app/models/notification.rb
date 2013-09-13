dd841b4fc219272f9e85db7a37b0d3024489fdd0
Nick Harvey <nickharveyonline@gmail.com>
2013-09-13 13:16:43 -0400

    fields created for models

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,12 @@
 class Notification
   include Mongoid::Document
+
+  field :type, :String
+  field :body, :String
+  field :subject, :String, :default => ""
+  field :conversation_id, :Integer
+  field :draft, :boolean, :default => false
+  field :updated_at, :DateTime, :null => false
+  field :created_at, :DateTime, :null => false
+
 end
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,3 +1,12 @@
 class Receipt
   include Mongoid::Document
+
+  field :notification_id, :integer, :null => false
+  field :read, :boolean, :default => false
+  field :trashed, :boolean, :default => false
+  field :deleted, :boolean, :default => false
+  field :mailbox_type, :string, :limit => 25
+  field :created_at, :DateTime, :null => false
+  field :updated_at, :DateTime, :null => false
+
 end
